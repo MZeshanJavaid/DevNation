a43290d3a395943f17dde68e81f2a7d018ddca0e
DaXM10 <103646549+MZeshanJavaid@users.noreply.github.com>
2022-04-14 21:53:51 +0500

    New Updates

--- whatsappui/lib/main.dart
+++ whatsappui/lib/main.dart
@@ -11,105 +11,369 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Widget openPopUp() {
+    return PopupMenuButton(
+      itemBuilder: (context) {
+        return List.generate(
+            3,
+            (index) => const PopupMenuItem(
+                  child: Text('Setting'),
+                ));
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          actions: [
+            // Widget for the search
+            const Icon(Icons.search),
+            // Widget for implementing the three-dot menu
+            PopupMenuButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              itemBuilder: (context) {
+                return [
+                  // In this case, we need 5 popupmenuItems one for each option.
+                  const PopupMenuItem(child: Text('New Group')),
+                  const PopupMenuItem(child: Text('New Broadcast')),
+                  const PopupMenuItem(child: Text('Linked Devices')),
+                  const PopupMenuItem(child: Text('Starred Messages')),
+                  const PopupMenuItem(child: Text('Settings')),
+                ];
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+          ],
+          backgroundColor: const Color(0xff128C7E),
+          title: const Text('WhatsApp'),
+          bottom: const TabBar(
+            indicatorSize: TabBarIndicatorSize.tab,
+            indicatorColor: Colors.white,
+            tabs: [
+              Tab(
+                  iconMargin: EdgeInsets.all(100),
+                  child: Icon(
+                    Icons.camera_alt_rounded,
+                  )),
+              Tab(
+                child: Text('CHATS', style: TextStyle(color: Colors.white)),
+              ),
+              Tab(
+                child: Text('STATUS', style: TextStyle(color: Colors.white)),
+              ),
+              Tab(
+                child: Text('CALLS', style: TextStyle(color: Colors.white)),
+              ),
+            ],
+            labelColor: Colors.white,
+          ),
+        ),
+
+        // ! THE DESIGNED BODY
+        body: const TabBarView(
+          children: [
+            Center(child: Text('This feature is coming soon')),
+            ChatsTab(),
+            Center(child: Text('Status feature is coming soon')),
+            Center(child: Text('Call feature is coming soon')),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class ChatsTab extends StatelessWidget {
+  const ChatsTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView(
+          children: const [
+            SingleChatWidget(
+                chatTitle: "Arya Stark",
+                chatMessage: 'I wish GoT had better ending',
+                seenStatusColor: Colors.blue,
+                imageUrl:
+                    'https://static-koimoi.akamaized.net/wp-content/new-galleries/2020/09/maisie-williams-aka-arya-stark-of-game-of-thrones-someone-told-me-in-season-three-that-i-was-going-to-kill-the-night-king001.jpg'),
+            SingleChatWidget(
+                chatTitle: "Robb Stark",
+                chatMessage: 'Did you check Maisie\'s latest post?',
+                seenStatusColor: Colors.grey,
+                imageUrl:
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDXCC-UB67rk0HtbmrDvVsIGvnPfTAMc_tSg&usqp=CAU'),
+            SingleChatWidget(
+                chatTitle: "Jaqen H'ghar",
+                chatMessage: 'Valar Morghulis',
+                seenStatusColor: Colors.grey,
+                imageUrl:
+                    'https://static3.srcdn.com/wordpress/wp-content/uploads/2017/06/Jaqen-Hghar-Game-of-Thrones.jpg'),
+            SingleChatWidget(
+                chatTitle: "Sansa Stark",
+                chatMessage: 'The North Remembers',
+                seenStatusColor: Colors.blue,
+                imageUrl:
+                    'https://i.insider.com/5ce420e193a15232821d3084?width=700'),
+            SingleChatWidget(
+                chatTitle: "Jon Snow",
+                chatMessage: 'Stick em\' with the pointy end',
+                seenStatusColor: Colors.grey,
+                imageUrl:
+                    'https://i.insider.com/5cb3c8e96afbee373d4f2b62?width=700'),
+            SingleChatWidget(
+                chatTitle: "Arya Stark",
+                chatMessage: 'I wish GoT had better ending',
+                seenStatusColor: Colors.blue,
+                imageUrl:
+                    'https://static-koimoi.akamaized.net/wp-content/new-galleries/2020/09/maisie-williams-aka-arya-stark-of-game-of-thrones-someone-told-me-in-season-three-that-i-was-going-to-kill-the-night-king001.jpg'),
+            SingleChatWidget(
+                chatTitle: "Robb Stark",
+                chatMessage: 'Did you check Maisie\'s latest post?',
+                seenStatusColor: Colors.blue,
+                imageUrl:
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDXCC-UB67rk0HtbmrDvVsIGvnPfTAMc_tSg&usqp=CAU'),
+            SingleChatWidget(
+                chatTitle: "Jon Snow",
+                chatMessage: 'Stick em\' with the pointy end',
+                seenStatusColor: Colors.blue,
+                imageUrl:
+                    'https://i.insider.com/5cb3c8e96afbee373d4f2b62?width=700'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// Widget to define how a single chat widget would look like
+class SingleChatWidget extends StatelessWidget {
+  final String? chatMessage;
+  final String? chatTitle;
+  final Color? seenStatusColor;
+  final String? imageUrl;
+
+  const SingleChatWidget({
+    Key? key,
+    this.chatMessage,
+    this.chatTitle,
+    this.seenStatusColor,
+    this.imageUrl,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        CircleAvatar(
+          radius: 30,
+          backgroundImage: NetworkImage(imageUrl!),
+        ),
+        Expanded(
+          child: ListTile(
+              title: Text('$chatTitle',
+                  style: const TextStyle(fontWeight: FontWeight.w600)),
+              subtitle: Row(children: [
+                Icon(
+                  seenStatusColor == Colors.blue ? Icons.done_all : Icons.done,
+                  size: 15,
+                  color: seenStatusColor,
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 6.0),
+                    child: Text(
+                      '$chatMessage',
+                      style: const TextStyle(overflow: TextOverflow.ellipsis),
+                    ),
+                  ),
+                ),
+              ]),
+              trailing: Column(
+                children: const [
+                  Padding(
+                    padding: EdgeInsets.only(top: 8.0),
+                    child: Text(
+                      'Yesterday',
+                    ),
+                  ),
+                ],
+              ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class StatusTab extends StatelessWidget {
+  const StatusTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Stack(
+                  children: const [
+                    CircleAvatar(
+                      backgroundColor: Color(0xff128C7E),
+                      foregroundColor: Color(0xff128C7E),
+                      radius: 30,
+                      backgroundImage: AssetImage('lib/assets/images/Me.jpg'),
+                    ),
+                    Positioned(
+                      top: 40,
+                      left: 40,
+                      child: CircleAvatar(
+                        radius: 10,
+                        child: Icon(Icons.add, size: 20),
+                      ),
+                    ),
+                  ],
+                ),
+                const Expanded(
+                  child: ListTile(
+                    title: Text('My Status'),
+                    subtitle: Padding(
+                      padding: EdgeInsets.only(top: 2.0),
+                      child: Text('Tap to add status update'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 8.0, bottom: 8.0),
+              child: Text('Viewed updates',
+                  style: TextStyle(fontWeight: FontWeight.w400)),
+            ),
+            Row(
+              children: [
+                Stack(
+                  children: const [
+                    CircleAvatar(
+                      backgroundColor: Colors.grey,
+                      radius: 30,
+                      child: CircleAvatar(
+                        radius: 28,
+                        backgroundImage:
+                            AssetImage('lib/assets/images/Mountains.jpg'),
+                      ),
+                    ),
+                  ],
+                ),
+                const Expanded(
+                  child: ListTile(
+                    title: Text('Arya Stark'),
+                    subtitle: Padding(
+                      padding: EdgeInsets.only(top: 2.0),
+                      child: Text('7 minutes ago'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            // Since the ExpansionTile has top and bottom borders by default and we don't want that so we
+            //use Theme to override its dividerColor property
+            Theme(
+              data: ThemeData().copyWith(dividerColor: Colors.transparent),
+              child: const ExpansionTile(
+                textColor: Colors.black,
+                tilePadding: EdgeInsets.all(0.0),
+                title: Text('Muted updates',
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                    )),
+                children: [
+                  SingleStatusItem(
+                    statusTitle: 'Cersei Lannister',
+                    statusTime: '56 minutes ago',
+                    statusImage: 'lib/assets/images/Ansu.jpg',
+                  ),
+                  SingleStatusItem(
+                    statusTitle: 'Lyanna Mormont',
+                    statusTime: '2 minutes ago',
+                    statusImage: 'lib/assets/images/Ubuntu.png',
+                  ),
+                  SingleStatusItem(
+                    statusTitle: 'Daenerys Targaryen',
+                    statusTime: '12 minutes ago',
+                    statusImage: 'lib/assets/images/Mountains.jpg',
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SingleStatusItem extends StatelessWidget {
+  final String? statusTitle;
+  final String? statusTime;
+  final String? statusImage;
+  const SingleStatusItem({
+    Key? key,
+    this.statusTitle,
+    this.statusTime,
+    this.statusImage,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Stack(
+          children: [
+            CircleAvatar(
+              backgroundColor: Colors.grey,
+              radius: 30,
+              child: CircleAvatar(
+                radius: 28,
+                backgroundImage: AssetImage('$statusImage'),
+              ),
+            ),
+          ],
+        ),
+        Expanded(
+          child: ListTile(
+            title: Text('$statusTitle'),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(top: 2.0),
+              child: Text("$statusTime"),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
